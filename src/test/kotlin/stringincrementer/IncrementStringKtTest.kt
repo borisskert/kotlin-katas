e02ba565caf575c2c3446aeb1e599c65548610de
Boris Skert <boris.skert@gmail.com>
2022-07-03 09:16:38 +0200

    Solve 'String incrementer' kata (#1)

---
+++ src/test/kotlin/stringincrementer/IncrementStringKtTest.kt
@@ -0,0 +1,19 @@
+package stringincrementer
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class TestExample {
+    @Test
+    fun FixedTests() {
+        assertEquals(incrementString("foobar000"), "foobar001")
+        assertEquals(incrementString("foobar999"), "foobar1000")
+        assertEquals(incrementString("foobar00999"), "foobar01000")
+        assertEquals(incrementString("foo"), "foo1")
+        assertEquals(incrementString("foobar001"), "foobar002")
+        assertEquals(incrementString("foobar1"), "foobar2")
+        assertEquals(incrementString("1"), "2")
+        assertEquals(incrementString(""), "1")
+        assertEquals(incrementString("009"), "010")
+    }
+}
